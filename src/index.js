2ab32c17a7556b2cb5c2d8e4180e78c769949fc4
Yusuke Shibata <shibata@fata.io>
2017-07-29 10:05:03 +0900

    fix semaphore behaviour

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
-import EventEmitter from 'event-emitter'
+// only firable once
+class Signal {
+  constructor(fn) {
+    this._fn = fn
+    this._fired = false
+  }
+  fire() {
+    if(this._fired) return
+    this._fired = true
+    this._fn.apply(this, arguments)
+  }
+}
 
 export default class Lock {
   constructor(limit) {
     this._limit = limit || 1
     this._lock = 0
-    this._ee = new EventEmitter()
+    this._queue = []
   }
-  _waitUnlock(timeout) {
+  _wait(timeout) {
     return new Promise((resolve, reject) => {
-      if(timeout !== undefined) setTimeout(() => this._ee.emit('unlock', new Error('Timeout')), timeout)
-      this._ee.once('unlock', err => err ? reject(err) : resolve())
+      const signal = new Signal(err => err ? reject(err) : resolve())
+      this._queue.push(signal)
+      if(timeout !== undefined) setTimeout(() => signal.fire(new Error('Timeout')), timeout)
     })
   }
   isLocked() {
@@ -17,20 +29,22 @@ export default class Lock {
   }
   async tryLock(timeout) {
     try {
-      await this.lock(timeout||0)
+      await this.lock(timeout || 0)
       return true
     } catch(err) {
       return false
     }
   }
   async lock(timeout) {
-    if(this.isLocked()) await this._waitUnlock(timeout)
+    if(this.isLocked()) await this._wait(timeout)
     this._lock++
-    this._ee.emit('lock')
   }
   unlock() {
     if(this._lock <= 0) throw new Error('Already unlocked')
     this._lock--
-    this._ee.emit('unlock')
+    const signal = this._queue.shift()
+    if(signal) {
+      signal.fire()
+    }
   }
 }
